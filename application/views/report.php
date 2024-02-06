9cb27fbae43459348b6113262dd2f581be47cad8
ranidigitalwebsynation@gmail.com <ranimehta1910@gmail.com>
2024-02-06 16:42:31 +0530

    Changes

--- application/views/report.php
+++ application/views/report.php
@@ -23,11 +23,8 @@
    <section class="vs-blog-wrapper blog-details blog-wrap1 space-top space-extra-bottom" data-bg-src="assets/img/bg/blog-main-bg-2-1.jpg">
        <div class="container">
            <div class=" gx-40">
-
                <div class="vs-blog blog-style4">
-
                    <div class="blog-content">
-
                        <h2 class="blog-title">Report</h2>
                        <ul class="mt-4">
                            <li>
@@ -36,17 +33,9 @@
                            <li>
                                <p>The Brandvocates India Private Limited, a Corporate Training Institution, previously known as Public Dialogue Academy has organized a Management Development Training Workshop named Promoting the Professionalism and Excellence in the Executive Offices at Udaipur during 10-14th January 2023. This was a Five Days and Four Nights Training programme with well-structured sessions of board room training and site visits of vital places. The delegates arrived in the early morning of 10th January 2023 and departed on in the evening of 14th January 2023 and thus having substantial time of Five Full days for the training and development activities. The 20 Delegates of training enjoyed the training and expressed their utmost satisfaction over it. tAll the delegates participated in the training with all zeal, enthusiasm and excitement. The Attendance Sheet with signatures for all five days is attached herewith a piece of testimony. The participants exhibited their utmost discipline, punctuality and dedication in all the formal and informal sessions of training. </p>
                            </li>
-
-
                        </ul>
                    </div>
-
-
-
-
                </div>
-
-
            </div>
        </div>
    </section>
